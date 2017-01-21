de6be8e8a64b9a567bd18398711fe68d9e8b6857
Nico Mastrandrea <Nico Mastrandrea>
2017-01-21 18:14:47 +0100

    Added DELETE testing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -88,3 +88,34 @@ describe('GET /todos/:id', () => {
   });
 
 });
+
+describe('DELETE /todos/:id', () => {
+
+  it('should remove a todo', (done) => {
+    var hexID = todos[1]._id.toHexString();
+    request(app)
+    .delete(`/todos/${hexID}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexID);
+    }).end((err, res) => {
+      if(err){
+        return done(err);
+      }
+      Todo.findById(hexID).then((todo) => {
+        expect(todo).toNotExist();
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var badID = new ObjectID();
+    request(app).delete(`/todos/${badID}`).expect(404).end(done);
+  });
+
+  it('should return 404 if object id is not valid', (done) => {
+    request(app).delete(`/todos/123123123`).expect(404).end(done);
+  });
+
+});
