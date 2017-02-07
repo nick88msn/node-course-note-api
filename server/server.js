3d3514e37c214a7491b468480a24354e2f54ffee
Nico Mastrandrea <Nico Mastrandrea>
2017-02-07 19:31:43 +0100

    Add tests for DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -116,7 +116,6 @@ app.get('/users/me', authenticate,(req,res) =>{
     res.send(req.user);
 });
 
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) =>{
   var body = _.pick(req.body,['email', 'password']);
   User.findByCredentials(body.email, body.password).then((user)=>{
@@ -127,6 +126,14 @@ app.post('/users/login', (req, res) =>{
     res.status(400).send(e);
   });
 
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
